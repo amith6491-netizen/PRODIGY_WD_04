6f7c094174a0b0d54f7545e4951c026170ac3a7c
amith <amith6491@gamil.com>
2026-02-14 07:21:46 +0530

    some changes in CSS

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Mobile Menu Toggle
 function toggleMenu() {
     let menu = document.getElementById("nav-links");
-    menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
+    menu.style.display = (menu.style.display === "flex") ? "left" : "flex";
 }
 
 // Dark Mode Toggle
 function toggleTheme() {
-    document.body.classList.toggle("dark");
+    document.body.classList.toggle("light");
 }
